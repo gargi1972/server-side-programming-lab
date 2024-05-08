1b2ad8b9501bce3d7677a74141424bafa458ec1e
gargi1972 <140623887+gargi1972@users.noreply.github.com>
2024-05-09 01:17:44 +0530

    Add files via upload

---
+++ Server Side Assignment/server side/app/hotel.js
@@ -0,0 +1,6 @@
+
+
+    document.getElementById("bookingForm").addEventListener("submit", function(event) {
+      event.preventDefault(); 
+      setTimeout(function(){alert("You will be contacted shortly for further details");},1000)
+    });
