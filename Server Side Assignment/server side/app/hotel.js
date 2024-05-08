53a052f09655f296142ef010ef8e87a0584c30fa
gargi1972 <140623887+gargi1972@users.noreply.github.com>
2024-05-09 01:19:11 +0530

    Delete Server Side Assignment/server side directory

--- Server Side Assignment/server side/app/hotel.js
+++
@@ -1,6 +0,0 @@
-
-
-    document.getElementById("bookingForm").addEventListener("submit", function(event) {
-      event.preventDefault(); 
-      setTimeout(function(){alert("You will be contacted shortly for further details");},1000)
-    });
